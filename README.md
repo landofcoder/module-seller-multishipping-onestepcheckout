8bb61f8cd64747832e1c5182aba52defaa8632f2
landofcoder <landofcoder@gmail.com>
2021-09-10 16:04:11 +0700

    updated module files

---
+++ README.md
@@ -0,0 +1,8 @@
+# MAGENTO 2 Compatible Module between Seller Multi Shipping and OneStepCheckout extension
+
+The module will help resolve issue when use MultiShipping and OneStepCheckout extension when use together.
+
+## Requires extensions:
+- Landofcoder Marketplace Extension
+- Landofcoder Seller Multi Shipping Extension
+- Ecomteck OneStepCheckout Extension
---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,29 @@
+/**
+ * Landofcoder
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Landofcoder.com license that is
+ * available through the world-wide-web at this URL:
+ * https://landofcoder.com/license-agreement.html
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category   Landofcoder
+ * @package    Lofmp_MultiShippingOneStepCheckout
+ * @copyright  Copyright (c) 2021 Landofcoder (https://landofcoder.com/)
+ * @license    https://landofcoder.com/LICENSE-1.0.html
+ */
+
+var config = {
+    config: {
+        mixins: {
+            'Magento_Checkout/js/view/shipping': {
+                'Lofmp_MultiShippingOneStepCheckout/js/mixin/shipping-view-mixin': true
+            }
+        }
+    }
+};
